1bf5cf30f4453643b73c7fca0aeaecd2c141bfa6
raj <barath_rock@yahoo.co.in>
2017-07-12 16:55:18 -0400

    changes made to Greeter.java accor to suggestion given by stylecheck

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,6 +12,11 @@ public class Greeter {
 public Greeter() {
   }
 
+/**
+ * @param someone the name
+ * @return greeting string
+ */
+
 public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
